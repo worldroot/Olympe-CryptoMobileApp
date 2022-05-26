f1c705c33ca47410c8cc784d77565404cb6f38d0
Mehdi Behira <mehdibehira@gmail.com>
2022-05-27 01:31:53 +0200

    coin screen

--- lib/screens/first_screen.dart
+++ lib/screens/first_screen.dart
@@ -1,12 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:olympe/models/FetchCoins/exposed_coins_data_model.dart';
+import 'package:olympe/repository/repository.dart';
+import 'package:olympe/widgets/coin_list_widget.dart';
 
-class FirstScreen extends StatelessWidget {
+class FirstScreen extends StatefulWidget {
   const FirstScreen({
     Key? key,
   }) : super(key: key);
 
   @override
+  _FirstScreenState createState() => _FirstScreenState();
+}
+
+class _FirstScreenState extends State<FirstScreen> {
+  late Future<ExposedCoinsDataModel> _futureCoins;
+  late Repository repository;
+  @override
+  void initState() {
+    repository = Repository();
+    _futureCoins = repository.getCoins();
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return Container();
+    return FutureBuilder<ExposedCoinsDataModel>(
+      future: _futureCoins,
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          if (snapshot.hasData) {
+            var coinsData = snapshot.data!.dataModel;
+            return CoinListWidget(coins: coinsData);
+          } else if (snapshot.hasError) {
+            return Text('${snapshot.error}');
+          }
+        }
+
+        return const Center(
+          child: CircularProgressIndicator(),
+        );
+      },
+    );
   }
 }
