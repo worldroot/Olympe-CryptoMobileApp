54a990c09a5fca49bd75fe829547f93dd85860f5
Mehdi Behira <mehdibehira@gmail.com>
2022-06-16 21:37:18 +0200

    removing backend

--- backend/routes/navigation.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-
-router.get('/', function (req, res, next) {
-    res.render('front/index', { title: 'Guitar' })
-})
-
-module.exports = router;
--- backend/utils/enum.js
+++
@@ -1,8 +0,0 @@
-const UserRole = "user"
-const WorkerRole = "worker"
-const OwnerRole = "owner"
-
-module.exports = { UserRole, WorkerRole, OwnerRole }
-
-
-
